ec9b446a14f3615466097e336ce13d8bfdcb7120
Zyzous <160857304+Zyzous@users.noreply.github.com>
2024-02-22 08:16:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import os
+import time
+import glob
+import os
+from gtts import gTTS
+from PIL import Image
 
+st.title("Interfaces Multimodales.")
+image = Image.open('text_to_audio.png')
+
+st.image(image, width=200)
