c4ef27cdfd53bf26fae165bffd83fdad099dbc39
Zyzous <160857304+Zyzous@users.noreply.github.com>
2024-02-22 08:19:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ from gtts import gTTS
 from PIL import Image
 
 st.title("Interfaces Multimodales.")
-image = Image.open('text_to_audio.png')
+image = Image.open('todoeventos.png')
 
 st.image(image, width=200)
